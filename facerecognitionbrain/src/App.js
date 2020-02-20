bb058b4ce6abe3d1479f27e839c6853a579869bf
john jorjill <orgil9506@gmail.com>
2020-02-20 15:45:57 +0800

    ImageLinkForm component

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +11,7 @@ class App extends Component {
     <div className="App">
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* {
       <FaceRecognition />} */}
