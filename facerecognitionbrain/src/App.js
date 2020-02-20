77b0ad2875e533ebaee4122d2741f1784704631f
john jorjill <orgil9506@gmail.com>
2020-02-20 13:52:54 +0800

    Added Logo component

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
-
+import Logo from './components/Logo/Logo';
 import './App.css';
 
-function App() {
+class App extends Component {
+  render(){
   return (
     <div className="App">
       <Navigation />
-      {/* {<Logo />
+      <Logo />
       <ImageLinkForm />
+      {/* {
       <FaceRecognition />} */}
     </div>
   );
+}
 }
 
 export default App;
